6fecea0c4a828db166babefda79df604d19f030c
Zheludevaat <161398877+Zheludevaat@users.noreply.github.com>
2026-04-16 03:13:49 +0200

    Bump sw v9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Alexander's Transits PWA
-const CACHE_NAME = 'transits-v8';
+const CACHE_NAME = 'transits-v9';
 const URLS_TO_CACHE = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', event => {

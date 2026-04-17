342dc3877f2a8da1288e0358fc6d68bfe6626cc3
Zheludevaat <161398877+Zheludevaat@users.noreply.github.com>
2026-04-17 10:46:22 +0200

    v41: fix Alexander ASC (hour stored as local not UTC), migration for existing data

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Alexander's Transits PWA
-const CACHE_NAME = 'transits-v40';
+const CACHE_NAME = 'transits-v41';
 const URLS_TO_CACHE = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
